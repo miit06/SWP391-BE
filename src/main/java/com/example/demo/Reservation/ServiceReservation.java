1a1206896c4da02cce5f57476219805b127304cb
DungTMHE186884 <dungtmhe186884@fpt.edu.vn>
2025-02-26 17:06:41 +0700

    Reservation Added

---
+++ src/main/java/com/example/demo/Reservation/ServiceReservation.java
@@ -0,0 +1,31 @@
+package com.example.demo.Reservation;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import java.math.BigDecimal;
+
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@Table(name = "ReservationService")
+public class ServiceReservation {
+    @Column(name = "ReservationID")
+    private String ReservationID;
+
+    @Column(name = "ServiceID")
+    private String ServiceID;
+
+    @Column(name = "Quantity")
+    private int Quantity;
+
+    @Column(name = "NoP")
+    private Integer NoP;
+
+    @Column(name = "Cost")
+    private BigDecimal Cost;
+
+}
