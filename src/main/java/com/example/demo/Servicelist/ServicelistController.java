c01a0300a367f9133aee40342b9e006449da7c4b
khanhvqhe176783 <khanhvqhe176783@fpt.edu.vn>
2025-03-06 22:14:40 +0700

    servicelist+detail

--- src/main/java/com/example/demo/Servicelist/ServicelistController.java
+++ src/main/java/com/example/demo/Servicelist/ServicelistController.java
@@ -1,11 +1,7 @@
 package com.example.demo.Servicelist;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.domain.Page;
-import org.springframework.stereotype.Service;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
